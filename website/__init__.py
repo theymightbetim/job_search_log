3e3cc2e109e49dc32480dd0d7fad4bc2a8f6a8d0
Tim Mackenzie <theymightbetim@gmail.com>
2025-02-28 03:48:37 -0500

    database w00t

--- website/__init__.py
+++ website/__init__.py
@@ -1,9 +1,18 @@
-from flask import (Flask)
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+from flask_login import LoginManager
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
 
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'thisisasecretkey'  # TODO: move to env
+    app.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
 
     from .views import views
     from .auth import auth
@@ -11,4 +20,15 @@ def create_app():
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
+    from .models import User,Note,JobApplicationAudit,Status, JobType, Location, LocationType, Contact
+    
+    with app.app_context():
+        db.create_all()
+
     return app
+
+
+def create_database(app):
+    if not path.exists('website/' + DB_NAME):
+        db.create_all(app=app)
+        print('Created Database!')
--- website/models.py
+++ website/models.py
@@ -1,5 +1,62 @@
-from flask import Blueprint
+from . import db
+from flask_login import UserMixin
+import enum
+from sqlalchemy.sql import func
+from sqlalchemy import Enum, Column
+import typing
 
-views = Blueprint('views', __name__)
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String(150))
+    first_name = db.Column(db.String(150))
 
+class Note(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    data = db.Column(db.DateTime(timezone=True), default=func.now())
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))    
 
+class Status(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    application_status = db.Column(db.String(100))
+        
+class Contact(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    phone = db.Column(db.String(15))
+    email = db.Column(db.String(150))
+    company = db.Column(db.String(100))
+    
+class JobType(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    job_type: Column = db.Column(db.String(50))
+    
+class LocationType(enum.Enum):
+    remote = "Remote"
+    hybrid = "Hybrid"
+    on_site = "On Site"
+    
+class Location(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    city = db.Column(db.String(50))
+    state = db.Column(db.String(2))
+    location_type = db.Column(Enum(LocationType))
+    
+class JobApplication(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    job_company = db.Column(db.String(150))
+    job_listing = db.Column(db.Text)    
+    job_listing_url = db.Column(db.String(1000))
+    resume = db.Column(db.String(256))
+    job_type_id = db.Column(db.Integer, db.ForeignKey('job_type.id'))
+    location_id = db.Column(db.Integer, db.ForeignKey("location.id"))
+    contact_id = db.Column(db.Integer, db.ForeignKey("contact.id"))
+    status_id = db.Column(db.Integer, db.ForeignKey("status.id"))
+    updated = db.Column(db.DateTime(timezone=True), default=func.now())
+    
+class JobApplicationAudit(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    application_id = db.Column(db.Integer)
+    contact_id = db.Column(db.Integer, db.ForeignKey("contact.id"))
+    status_id = db.Column(db.Integer, db.ForeignKey("status.id"))
+    updated = db.Column(db.DateTime(timezone=True))
